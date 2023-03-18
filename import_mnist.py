8d774dfb9e9a69b655c4b950e56c14e34f57ba22
shivatmica <62459136+shivatmica@users.noreply.github.com>
2023-03-18 19:05:30 +0530

    magic number

--- import_mnist.py
+++ import_mnist.py
@@ -1,4 +1,5 @@
 from sklearn.datasets import fetch_openml
 from sklearn.metrics.pairwise import euclidean_distances
 X, _ = fetch_openml('mnist_784', version=1, return_X_y=True, as_frame=False)
-X = X[:10000]
+data_sample = 10000
+X = X[:data_sample]
