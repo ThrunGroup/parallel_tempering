d6eac16a1cb9dc7de31e189a3e0529a72e5a82c6
shivatmica <62459136+shivatmica@users.noreply.github.com>
2023-04-06 22:20:18 +0530

    Delete import_mnist.py

--- import_mnist.py
+++
@@ -1,9 +0,0 @@
-from sklearn.datasets import fetch_openml
-from sklearn.metrics.pairwise import euclidean_distances
-X, _ = fetch_openml("mnist_784", 
-                    version=1, 
-                    return_X_y=True, 
-                    as_frame=False
-                   )
-data_sample = 10000
-X = X[:data_sample]
